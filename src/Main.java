bc29c9df2e9b3981bf654a7df8f505048ff77c06
Anastasia <grraf.est@gmail.com>
2023-04-23 19:04:47 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 17426;
+        int one = 20;
+        int bonus = amount / one;
+
+
+        System.out.println("Миль будет начислено: " + bonus);
+    }
+}
